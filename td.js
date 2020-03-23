f5fbd5dcbf88e9d9c3071968bc838b6f9d164bb8
Geoffrey_max <geoffrey.clermont@ynov.com>
2020-03-23 11:34:53 +0100

    dernier commit

--- td.js
+++ td.js
@@ -6,7 +6,23 @@ db.createCollection("sport")
 
 db.sports.insertOne({ object: "ballon", nbPlayer: 11, coach: "Mr Parkinson", tournoi: true })
 
-db.sports.updateMany({}, { $set: { requiredTeam: true, title: "football" } })
-
 db.sports.insertMany([{ object: "ballon", nbPlayer: 8, coach: "Polnareff", tournoi: false }, { object: "raquette", nbPlayer: 2, coach: "Pique", tournoi: false }])
 
+db.sports.updateMany({}, { $set: { requiredTeam: true, title: "football" } })
+
+
+//Mettre un nouveau champ à ceux qui ont requiredTeam
+
+db.sports.updateMany({ requiredTeam: true }, { $set: { couleur: "rouge" } }, { upsert: true })
+
+//Mettre à jour tous les documents qui ont requiredTeam, en augmentant le  <nom de notre choix> par 10
+
+db.sports.updateMany({ requiredTeam: true }, { $inc: { nbPlayer: 10 } }, { upsert: true })
+
+//Ajouter un nouveau champ teams , pour les documents qui ont le requiredTeam
+
+db.sports.updateMany({ requiredTeam: true }, { $set: { team: [{ name: "Leo", nombre: "1" }, { name: "Geoffrey", nombre: "2" }] } }, { upsert: true })
+
+//Mettre à jour tous les elements de teams, avec le nouveau champ titulaire
+
+db.sports.updateMany({ team: { $exists: true } }, { $set: { "team.$[].titulaire": true } }, { upsert: true })
